4b1ac71b0525b48a0806b34c6d9b88e0d7bd68ba
oneartplease <oneartplease@46015a06-04f4-4860-9c33-90e0f15d9639>
2012-04-02 09:09:26 +0000

    diegos fix for displaying image icons
    git-svn-id: https://elphel.svn.sourceforge.net/svnroot/elphel/Apertus/ElphelVision/Eclipse_Project@342 46015a06-04f4-4860-9c33-90e0f15d9639

--- src/VLCPlayer.java
+++ src/VLCPlayer.java
@@ -100,7 +100,7 @@ public class VLCPlayer {
     }
 
     public void PlayFakeStream() {
-	mediaPlayer.playMedia("fake:// --fake-file novideo.png");
+	mediaPlayer.playMedia("fake:// --fake-file novideo.jpg");
     }
 
     /*
